cdd7c9cecb676bdc3ee540cb2cbff35d4ac2c895
Dizzykong <89928208+Dizzykong@users.noreply.github.com>
2021-12-04 13:49:44 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@
   <footer class="bg-light text-center text-lg-start">
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-      <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+      <a class="text-dark" href="https://github.com/Dizzykong/CreativeProject3-2.git">Github Repository Link</a>
     </div>
     <!-- Copyright -->
   </footer>
